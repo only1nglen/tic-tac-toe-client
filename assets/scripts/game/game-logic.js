0002baf8e20a7008b0f3479f7ecfe0416235d4ca
only1nglen <only1nglen@gmail.com>
2020-09-04 12:37:05 -0400

    Refactored game-logic

--- assets/scripts/game/game-logic.js
+++ assets/scripts/game/game-logic.js
@@ -10,64 +10,44 @@ const endGame = () => {
   store.gameOver = true
 }
 
-const checkWinner =  function (movesMade) {
-  if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
-    endGame()
-  } else if
-    (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
-    endGame()
-  } else if
-    (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
-      endGame()
-  } else if
-    (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
-      endGame()
-  } else if
-    (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
-      endGame()
-  }  else if
-    (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
-      endGame()
-  } else if
-    (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
-      endGame()
-  } else if
-    (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
-      endGame()
-   } else if (store.movesMade === 9 && store.gameOver === false ) {
-      $('.message').text('Game is a Draw')
-      $('.player').text('Create a New Game')
-      store.gameOver = true
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ]
+
+  const checkWinner =  function () {
+    winningCombos.forEach(combo => {
+      if (gameBoard[combo[0]] !== '' && gameBoard[combo[0]] === gameBoard[combo[1]] && gameBoard[combo[0]] === gameBoard[combo[2]])
+          endGame()
+      })
+      if (store.movesMade === 9 && store.gameOver === false ) {
+        $('.message').text('Game is a Draw')
+        $('.player').text('Create a New Game')
+        store.gameOver = true
+      }
+      else if (store.gameOver === false) {
+        $('.message').text("You've made a move!")
+        store.gameOver = false
+      }
     }
-    else {
-      $('.message').text("You've made a move!")
-      store.gameOver = false
-    }
-  }
 
 const clearBoard = function () {
   $('.box').text('')
 }
 
 const collectMove = function () {
-  const boxPicked0 = document.getElementById("box0").innerHTML
-  const boxPicked1 = document.getElementById("box1").innerHTML
-  const boxPicked2 = document.getElementById("box2").innerHTML
-  const boxPicked3 = document.getElementById("box3").innerHTML
-  const boxPicked4 = document.getElementById("box4").innerHTML
-  const boxPicked5 = document.getElementById("box5").innerHTML
-  const boxPicked6 = document.getElementById("box6").innerHTML
-  const boxPicked7 = document.getElementById("box7").innerHTML
-  const boxPicked8 = document.getElementById("box8").innerHTML
-  gameBoard.splice( 0, 1, boxPicked0)
-  gameBoard.splice( 1, 1, boxPicked1)
-  gameBoard.splice( 2, 1, boxPicked2)
-  gameBoard.splice( 3, 1, boxPicked3)
-  gameBoard.splice( 4, 1, boxPicked4)
-  gameBoard.splice( 5, 1, boxPicked5)
-  gameBoard.splice( 6, 1, boxPicked6)
-  gameBoard.splice( 7, 1, boxPicked7)
-  gameBoard.splice( 8, 1, boxPicked8)
+  let boardState = []
+  for (let i = 0; i < 9; i++) {
+    const boxPicked = document.getElementById("box"+i).innerHTML
+    boardState.push(boxPicked)
+    gameBoard.splice(i, 1, boardState[i])
+  }
 }
 
 module.exports = {
